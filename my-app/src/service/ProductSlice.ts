7841b6c26b8e53fd87ff350d69ecf2759a89cb79
유영빈 <97148877+BTSBRINGMEHERE@users.noreply.github.com>
2023-03-06 14:59:45 +0900

    test
    vercel test

--- my-app/src/service/ProductSlice.ts
+++ my-app/src/service/ProductSlice.ts
@@ -1,28 +1,28 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import axios from "axios";
-import { ProductType } from "types/product";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+import { ProductType } from "types/product"
 
-const create = axios.create();
-const API_URL = process.env.REACT_APP_API_URL as string;
+const create = axios.create()
+const API_URL = "https://fakestoreapi.com/products"
 
 export const fetchProductAPI = async () => {
-	const BASE_URL = API_URL;
-	const { data } = await create.get<ProductType>(BASE_URL);
-	return data;
-};
+	const BASE_URL = API_URL
+	const { data } = await create.get<ProductType>(BASE_URL)
+	return data
+}
 
 export const fetchProduct = createAsyncThunk("product", async () => {
-	const response = await fetchProductAPI();
-	return response;
-});
+	const response = await fetchProductAPI()
+	return response
+})
 
 interface ProductState {
-	products: ProductType;
+	products: ProductType
 }
 
 const initialState = {
 	products: [],
-} as ProductState;
+} as ProductState
 
 export const productSlice = createSlice({
 	name: "products",
@@ -32,10 +32,10 @@ export const productSlice = createSlice({
 		builder.addCase(
 			fetchProduct.fulfilled,
 			(state, action: PayloadAction<ProductType>) => {
-				state.products = action.payload;
+				state.products = action.payload
 			}
-		);
+		)
 	},
-});
+})
 
-export const productReducer = productSlice.reducer;
+export const productReducer = productSlice.reducer
